fcb0fba4896dca16f123c5afee3a2f2fc07ca364
Vishal Mane <vishal12345mane00@gmail.com>
2024-08-10 19:43:11 +0530

    nothing

--- src/main/java/com/Backend_CRUD/Entity/Category.java
+++ src/main/java/com/Backend_CRUD/Entity/Category.java
@@ -2,7 +2,7 @@ package com.Backend_CRUD.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
